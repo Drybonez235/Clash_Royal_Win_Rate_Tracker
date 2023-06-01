86e7e41643998edb528e114df9821cce28c88a9a
Jonathan Lewis <jonathan.lewis@welch.edu>
2023-05-31 21:05:51 -0500

    Forget the last commit, this one is 100 times more simple. THANK YOU MIDDLEWARE!

--- Clash_Royal_Server.js
+++ Clash_Royal_Server.js
@@ -4,21 +4,29 @@ const app = express();
 const hostname = '127.0.0.1';
 const port = 8080;
 
-
-//res is the callback. It will fire
-app.post('/', /*this is the start of the arrow function that has 2 parameters, req and res*/(req, res, next) => {
-    
-    /*this is the main function body!*/
+const set_headers = (req, res, next) => {
     res.setHeader('Content-Type','text/plain');
     res.setHeader('Access-Control-Allow-Origin', "*");
     res.setHeader('Access-Control-Allow-Methods', "OPTIONS, POST, GET");
+    next();
+}
+
+const function1 = (req, res, next) => {
     console.log("function 1");
+    next();
+}
+
+const function2 = (req, res, next) => {
+    console.log("function 2");
     res.send('Hello World!')
-    next()
-    }/*This is the res arrow function body end bracket*/
-         ,(req, res) => {
-            console.log("function 2")
-            }/*This is the next end function bracket*/
-         )/*This is the app.post end function parathesis*/;
+}
+
+app.use(set_headers)
+app.use(function1)
+app.use(function2)
+
+app.post('/', (req, res) => {
+    
+    });
 
 app.listen(port, () => console.log('Server ready'));
